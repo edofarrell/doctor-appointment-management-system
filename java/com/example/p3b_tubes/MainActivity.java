e366955aed0a994802d4e31f395f78fa2d0b26d4
edofarrell <edo.farrell17@gmail.com>
2022-10-14 22:25:04 +0700

    class doctor + navigation

--- java/com/example/p3b_tubes/MainActivity.java
+++ java/com/example/p3b_tubes/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.p3b_tubes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentResultListener;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding activityMainBinding;
     private HashMap<String, Fragment> fragments;
     private FragmentManager fm;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = this.activityMainBinding.toolbar;
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = this.activityMainBinding.getRoot();
+        drawer = this.activityMainBinding.getRoot();
         ActionBarDrawerToggle abdt = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open_drawer, R.string.close_drawer);
         drawer.addDrawerListener(abdt);
         abdt.syncState();
@@ -37,11 +40,59 @@ public class MainActivity extends AppCompatActivity {
         this.fragments = new HashMap<>();
         //tambah fragment di sini
         this.fragments.put("home", HomeFragment.newInstance());
+        this.fragments.put("appointment", AppointmentFragment.newInstance());
+//        this.fragments.put("doctor", DoctorFragment.newInstance());
         this.fm =getSupportFragmentManager();
 
         FragmentTransaction ft = this.fm.beginTransaction();
         ft.add(this.activityMainBinding.fragmentContainer.getId(), fragments.get("home"))
                 .addToBackStack(null)
                 .commit();
+
+        this.fm.setFragmentResultListener("changePage", this, new FragmentResultListener() {
+            @Override
+            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+                String page = result.getString("page");
+                changePage(page);
+            }
+        });
+    }
+
+    private void changePage(String page) {
+        FragmentTransaction ft = this.fm.beginTransaction();
+        if(page.equals("home")){
+            if(this.fragments.get("home").isAdded()){
+                ft.show(this.fragments.get("home"));
+            }else{
+                ft.add(this.activityMainBinding.fragmentContainer.getId(), this.fragments.get("home"))
+                        .addToBackStack(null);
+            }
+            if(this.fragments.get("appointment").isAdded()) ft.hide(this.fragments.get("appointment"));
+//            if(this.fragments.get("doctor").isAdded()) ft.hide(this.fragments.get("doctor"));
+
+        }else if(page.equals("appointment")){
+            if(this.fragments.get("appointment").isAdded()){
+                ft.show(this.fragments.get("appointment"));
+            }else{
+                ft.add(this.activityMainBinding.fragmentContainer.getId(), this.fragments.get("appointment"));
+            }
+            if(this.fragments.get("home").isAdded()) ft.hide(this.fragments.get("home"));
+//            if(this.fragments.get("doctor").isAdded()) ft.hide(this.fragments.get("doctor"));
+
+        }else if(page.equals("doctor")){
+            if(this.fragments.get("doctor").isAdded()){
+                ft.show(this.fragments.get("doctor"));
+            }else{
+                ft.add(this.activityMainBinding.fragmentContainer.getId(), this.fragments.get("doctor"));
+            }
+            if(this.fragments.get("home").isAdded()) ft.hide(this.fragments.get("home"));
+            if(this.fragments.get("appointment").isAdded()) ft.hide(this.fragments.get("appointment"));
+
+        }else if(page.equals("exit")){
+            this.moveTaskToBack(true);
+            this.finish();
+        }
+        ft.commit();
+        this.drawer.closeDrawers();
     }
 }
--- java/com/example/p3b_tubes/NavigationFragment.java
+++ java/com/example/p3b_tubes/NavigationFragment.java
@@ -18,6 +18,26 @@ public class NavigationFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         this.fragmentNavigationBinding = FragmentNavigationBinding.inflate(inflater);
+
+        this.fragmentNavigationBinding.tvHome.setOnClickListener(this::onClickChangePage);
+        this.fragmentNavigationBinding.tvAppointment.setOnClickListener(this::onClickChangePage);
+        this.fragmentNavigationBinding.tvDoctor.setOnClickListener(this::onClickChangePage);
+        this.fragmentNavigationBinding.tvExit.setOnClickListener(this::onClickChangePage);
+
         return this.fragmentNavigationBinding.getRoot();
+    }
+
+    private void onClickChangePage(View view) {
+        Bundle result = new Bundle();
+        if (view==this.fragmentNavigationBinding.tvHome){
+            result.putString("page", "home");
+        }else if (view==this.fragmentNavigationBinding.tvAppointment){
+            result.putString("page", "appointment");
+        }else if (view==this.fragmentNavigationBinding.tvDoctor){
+            result.putString("page", "doctor");
+        }else if(view == this.fragmentNavigationBinding.tvExit){
+            result.putString("page", "exit");
+        }
+        getParentFragmentManager().setFragmentResult("changePage", result);
     }
 }
