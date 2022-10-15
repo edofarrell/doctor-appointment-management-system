a4ceff8b113ec9f34b51458770e2e925dbe2bf42
vitofevrier <vitofev15202@gmail.com>
2022-10-15 10:37:42 +0700

    DoctorListAdapter

---
+++ java/com/example/p3b_tubes/DoctorListAdapter.java
@@ -0,0 +1,65 @@
+package com.example.p3b_tubes;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.p3b_tubes.databinding.ItemListDoctorBinding;
+
+import java.util.ArrayList;
+
+public class DoctorListAdapter extends BaseAdapter {
+    private ArrayList<Doctor> doctorList;
+
+    private class ViewHolder{
+        protected int i;
+        protected TextView tvDoctor;
+        protected TextView tvSpecialty;
+
+        public ViewHolder(ItemListDoctorBinding itemListDoctorBinding, int i) {
+            this.i = i;
+            tvDoctor = itemListDoctorBinding.tvDoctorName;
+            tvSpecialty = itemListDoctorBinding.tvDoctorSpecialty;
+        }
+
+        private void updateView(int i) {
+            Doctor doctor = doctorList.get(i);
+            tvDoctor.setText(doctor.getName());
+            tvDoctor.setText(doctor.getSpecialty());
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return doctorList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return doctorList.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        ItemListDoctorBinding itemListDoctorBinding = ItemListDoctorBinding.inflate(inflater);
+        ViewHolder viewHolder;
+        if(view == null) {
+            view = itemListDoctorBinding.getRoot();
+            viewHolder = new ViewHolder(itemListDoctorBinding, i);
+            view.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) view.getTag();
+        }
+        viewHolder.updateView(i);
+
+        return view;
+    }
+}
